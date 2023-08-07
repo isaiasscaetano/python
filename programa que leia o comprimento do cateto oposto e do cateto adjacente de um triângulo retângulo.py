4b1d094faa166e88780b010ac260c862660764ce
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2023-08-07 11:43:08 -0300

    Add files via upload

---
+++ programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo.py
@@ -0,0 +1,18 @@
+'''ko = float(input('comprimento do cateto oposto:'))
+ka = float(input('comprimento do cateto adiacente:'))
+ga = (ko ** + ka ** 2) ** (1/2)
+print('a hipotenusa vai medir {:.2f}'.format(ga))'''
+
+
+'''import math
+go = float(input('composto do cateto oposto:'))
+ga = float(input('composto do cateto adiacente:'))
+gu = math.hypot(go, ga)
+print('hipotenusa vai medir {:.2}'.format(gu))'''
+
+
+from math import hypot
+du = float(input('comprimento do cateto oposto:'))
+da = float(input('comprimento do cateto oposto:'))
+de = hypot(du, da)
+print('a hipotenusa vai medir. {:.2}'.format(de))
