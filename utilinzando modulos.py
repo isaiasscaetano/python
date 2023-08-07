a86e441531306756801d42e13db4d18e487f10d3
Isaias Caetano <131115372+isaiasscaetano@users.noreply.github.com>
2023-08-07 11:14:48 -0300

    Add files via upload

---
+++ utilinzando modulos.py
@@ -0,0 +1,14 @@
+num = float(input('coloque uns numeros'))
+print('os numeros foi {} e o numero inteiro é {}'.format(num, int(num)))
+
+'''from math import trunc 
+num = float(input('digite um numero:'))
+print ('0 numero digitado foi {} e o seu numero inteiro é {}'.format(num, trunc(num)))'''
+
+
+
+'''import math
+num = float(input('digite um numero:'))
+print ('0 numero digitado foi {} e o seu numero inteiro é {}'.format(num, math.trunc(num)))'''
+
+
